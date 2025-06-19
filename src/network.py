dd50221fe0ec2cb55a6d4cd60c77f37171bdcf4b
wilkertl <wilkertyller17@gmail.com>
2025-06-19 17:23:19 -0300

    Adding hosts and bandwidth variation

--- src/network.py
+++ src/network.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -18,7 +18,12 @@ CONTROLERS = ["172.17.0.5", "172.17.0.6", "172.17.0.7"]
 
 # Configurações para topologia GML
 MAX_BACKBONE_BW_GBPS = 10.0
+MIN_BACKBONE_BW_MBPS = 100.0  # Banda mínima entre switches (100 Mbps)
+MAX_BACKBONE_BW_MBPS = 10000.0  # Banda máxima entre switches (10 Gbps)
 PROPAGATION_SPEED_KM_PER_MS = 200  # Velocidade da luz na fibra
+MIN_HOSTS_PER_EDGE_SWITCH = 1
+MAX_HOSTS_PER_EDGE_SWITCH = 7
+EDGE_SWITCH_DEGREE_THRESHOLD = 2  # Switches com grau <= 2 são considerados de borda
 
 def haversine_distance(lat1, lon1, lat2, lon2):
     """Calcula a distancia geodesica em km."""
@@ -60,7 +65,7 @@ class Tower(Topo):
             self.addLink(leaf, spines[1])
 
             for j in range(1, 6):
-                host = self.addHost(f'h{j + i*5}')
+                host = self.addHost('h{}'.format(j + i*5))
                 self.addLink(host, leaf)
 
 class GmlTopo(Topo):
@@ -76,15 +81,56 @@ class GmlTopo(Topo):
         except Exception:
             G = nx.read_gml(gml_file, label='label')
 
-        info("*** Adicionando switches e hosts...\n")
+        info("*** Adicionando switches...\n")
         switches = {}
+        self.edge_switches = []
+        self.backbone_switches = []
+        
+        # Primeiro, adicione todos os switches
         for node_id, node_data in G.nodes(data=True):
             switch_name = str(node_id)
             switches[node_id] = self.addSwitch(switch_name, protocols='OpenFlow13')
+
+        # Classifica switches como edge ou backbone baseado no grau (número de conexões)
+        info("*** Classificando switches...\n")
+        for node_id, switch_obj in switches.items():
+            switch_degree = G.degree(node_id)
+            if switch_degree <= EDGE_SWITCH_DEGREE_THRESHOLD:
+                self.edge_switches.append({
+                    'id': node_id,
+                    'name': switch_obj.name,
+                    'degree': switch_degree
+                })
+            else:
+                self.backbone_switches.append({
+                    'id': node_id,
+                    'name': switch_obj.name,
+                    'degree': switch_degree
+                })
+
+        info("*** Switches de BORDA ({}): {}\n".format(
+            len(self.edge_switches), 
+            [s['name'] for s in self.edge_switches]
+        ))
+        info("*** Switches de BACKBONE ({}): {}\n".format(
+            len(self.backbone_switches),
+            [s['name'] for s in self.backbone_switches]
+        ))
+
+        # Adiciona hosts apenas aos switches de borda com distribuição uniforme (1-7)
+        info("*** Adicionando hosts aos switches de borda...\n")
+        total_hosts = 0
+        for edge_switch in self.edge_switches:
+            num_hosts = random.randint(MIN_HOSTS_PER_EDGE_SWITCH, MAX_HOSTS_PER_EDGE_SWITCH)
+            info("Adicionando {} hosts ao switch de borda {}\n".format(num_hosts, edge_switch['name']))
             
-            host_name = "h{}".format(switch_name)
-            host = self.addHost(host_name)
-            self.addLink(host, switches[node_id], bw=100)
+            for i in range(num_hosts):
+                host_name = "h{}-s{}".format(total_hosts + 1, edge_switch['name'])
+                host = self.addHost(host_name)
+                self.addLink(host, switches[edge_switch['id']], bw=100)
+                total_hosts += 1
+
+        info("*** Total de hosts adicionados: {}\n".format(total_hosts))
 
         info("*** Adicionando links entre switches com parametros de rede...\n")
         for u, v in G.edges():
@@ -98,7 +144,10 @@ class GmlTopo(Topo):
             except KeyError:
                 delay_ms = 1.0
 
-            bw_mbps = random.uniform(100.0, MAX_BACKBONE_BW_GBPS * 1000)
+            # Usar distribuição uniforme para banda entre switches
+            bw_mbps = random.uniform(MIN_BACKBONE_BW_MBPS, MAX_BACKBONE_BW_MBPS)
+            info("Link {}-{}: BW={:.1f}Mbps, Delay={:.2f}ms\n".format(
+                switches[u].name, switches[v].name, bw_mbps, delay_ms))
 
             self.addLink(
                 switches[u], 
@@ -117,7 +166,7 @@ def run(topo):
 
     # Adiciona múltiplos controladores
     for i, ip in enumerate(CONTROLERS):
-        name = f"c{i}"
+        name = "c{}".format(i)
         c = RemoteController(name, ip=ip, port=6653)
         net.addController(c)
 
@@ -127,6 +176,13 @@ def run(topo):
     # Descobrindo todos os hosts
     for host in net.hosts:
         host.cmd("ping -c1 10.0.0.1 &")
+
+    # Exibe informações da topologia após inicialização
+    if hasattr(topo, 'edge_switches') and hasattr(topo, 'backbone_switches'):
+        info("*** RESUMO DA TOPOLOGIA ***\n")
+        info("Switches de BORDA: {}\n".format([s['name'] for s in topo.edge_switches]))
+        info("Switches de BACKBONE: {}\n".format([s['name'] for s in topo.backbone_switches]))
+        info("Total de hosts: {}\n".format(len(net.hosts)))
 
     return net
 
@@ -143,4 +199,9 @@ if __name__ == '__main__':
     # Para usar arquivo GML:
     LOCAL_GML_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tata_nld.gml')
     net = run(GmlTopo(gml_file=LOCAL_GML_FILE))
+
+    info("*** Iniciando CLI...\n")
+    CLI(net)
+    info("*** Parando a rede...\n")
+    net.stop()
 
